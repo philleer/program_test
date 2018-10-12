5c30690b5582994a3512adab533dd5de9748a415
PhilLeer <phillee2016@163.com>
2018-10-12 15:35:14 +0800

    Beginning of my github travelling

---
+++ travel-sites/calc.py
@@ -0,0 +1,16 @@
+
+def add(x,y):
+    return (x+y)
+
+def subtract(x,y):
+    return (x-y)
+
+def multiple(x,y):
+    return (x*y)
+
+def divide(x,y):
+    return (x/y)
+
+def square(x,y):
+    pass
+
