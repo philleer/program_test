cb8b37cf7c922ecfae4994b722e48dbb86894595
PhilLeer <phillee2016@163.com>
2018-10-17 22:17:00 +0800

    add definition of class thistest

--- hello/printtest.h
+++ hello/printtest.h
@@ -5,6 +5,22 @@
 #include <vector>
 #include <iterator>
 
+#define TEST_NAMESPACE_BEGIN namespace test {
+#define TEST_NAMESPACE_END }
+
+class thistest
+{
+public:
+	thistest(std::string str):name(str) {};
+	~thistest() {
+		std::cout << "thistest deconstruct!" << std::endl;
+	};
+	std::string getname();
+
+private:
+	std::string name;
+};
+
 class printtest
 {
 public:
@@ -19,7 +35,82 @@ private:
 	size_t y;
 };
 
-template<typename ForwardIterator> 
-void square(ForwardIterator first, ForwardIterator last);
+template<typename ForwardIterator>
+void square(ForwardIterator first, ForwardIterator last) {
+		std::cout << "  Square: ";
+		for ( ; first != last; first++ ) {
+				*first = (*first) * (*first);
+				std::cout << *first << " ";
+		}
+		std::cout << std::endl;
+}
+
+// Quene -- Template declaration
+template<typename T>
+class Quene {
+public:
+	Quene();
+	~Quene();
+	void push(T e);
+	T pop();
+
+private:
+	struct node {
+		T data;
+		node* next;
+	};
+	typedef node NODE;
+	NODE* mHead;
+};
+
+// Quene -- Template definition
+template<typename T>
+Quene<T>::Quene() {
+	mHead = NULL;
+}
+
+template<typename T>
+Quene<T>::~Quene() {
+	NODE* tmp;
+	while (mHead) {
+		tmp = mHead;
+		mHead = mHead->next;
+		delete tmp;
+	}
+}
+
+template<typename T>
+void Quene<T>::push(T e) {
+	NODE *ptr = new node;
+	ptr->data = e;
+	ptr->next = NULL;
+	if (mHead == NULL) {
+		mHead = ptr;
+		return;
+	}
+	NODE *cur = mHead;
+	while (cur) {
+		if (cur->next == NULL) {
+			cur->next = ptr;
+			return;
+		}
+		cur = cur->next;
+	}
+}
+
+template<typename T>
+T Quene<T>::pop() {
+	if (mHead == NULL) {
+		return NULL;
+	}
+	NODE *tmp = mHead;
+	T d = mHead->data;
+	mHead = mHead->next;
+	delete tmp;
+	return d;
+}
+
+void fun1();
+void fun2();
 
 #endif /* ifndef PRINTTEST_H */
