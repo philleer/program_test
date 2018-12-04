6f5f7bd1f246c91c6ac18594cf91dc174992c006
PhilLeer <phillee2016@163.com>
2018-12-04 13:53:43 +0800

    test some function in the bash script

--- bashfiles/hello_world.sh
+++ bashfiles/hello_world.sh
@@ -1,3 +1,33 @@
-echo Hello world! ;
-ls -lht ;
-echo "I'm here!"
+echo Hello world!
+date_string1=`date`
+date_str1=${date_string1// /}
+year1=${date_str1#*CST}
+name=$0
+filename=${name%.*}
+logn="log_"${filename}_${year1}$1".txt"
+
+(
+var=http://www.aaa.com/123.html
+vartmp="http://www.aaa.com/123.html"
+var1=${var#*//}
+var2=${var:0-8:3}
+echo $var--$var1--$vartmp
+echo $var2
+date
+date_string=`date`
+date_str=${date_string// /}
+year=${date_str#*CST}
+echo $year) | tee $logn
+
+# (time (../hello/build/hello) | tee log_time.txt) 2>> log_time.txt
+
+# (time (
+# START_TIME=$(date +%s.%N)
+# ../hello/build/hello
+# END_TIME=$(date +%s.%N)
+# ELAPSED_TIME=$(echo "$END_TIME-$START_TIME" | bc)
+# 
+# echo "Totally" $ELAPSED_TIME "s took." 
+# echo -e "\n"
+# ) | tee $LOG) 2>> $LOG
+
