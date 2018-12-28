4f647cdfde6f322fa4cfb80c8d0d7daf4aaec69d
PhilLeer <phillee2016@163.com>
2018-12-28 22:29:34 +0800

    move disimg to opencv_test dir

---
+++ opencv_test/disimg/build/CMakeFiles/disimg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disimg.dir/disimg.cc.o"
+  "disimg.pdb"
+  "disimg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/disimg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
